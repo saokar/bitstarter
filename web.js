544504d55453cfe69978611067945d4cd7158bff
Ubuntu <ubuntu@ip-10-166-105-188.us-west-1.compute.internal>
2013-07-07 06:34:23 +0000

     j[3~OBu <ubuntu@ip-10-166-105-188.us-west-1.compute.internal> :q!gitanch master

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var text = fs.readFileSync('./index.html','utf8')
+  //response.send('Hello World2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
